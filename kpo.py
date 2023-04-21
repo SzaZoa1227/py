2a467859c1cadae0220c614d6373ac89e3b89926
SzaZoa1227 <szabozoard27@gmail.com>
2023-04-21 08:20:21 +0200

    aprosagok

--- kpo.py
+++ kpo.py
@@ -1,21 +1,27 @@
 import random
-
+import time
+import os
 lehetosegek = ["kő", "papír", "olló"]
 pontok = [0, 0]
 
 while True:
+    os.system('cls')
     jatekos = input("Kő, papír vagy olló? ").lower()
     gep = random.choice(lehetosegek)
 
     if jatekos not in lehetosegek:  # jatekos valasztasa nincsen a lehetosegek kozott
         print("Érvénytelen választás!")
+        time.sleep(2)
         continue
 
     print("Te választásod:", jatekos)
-    print("A gép választása", gep)
+    time.sleep(1)
+    print("A gép választása:", gep)
 
     if jatekos == gep:
+        time.sleep(2)
         print("Döntetlen!")
+        time.sleep(2)
     elif (
         jatekos == "kő"
         and gep == "olló"
@@ -24,9 +30,11 @@ while True:
         or jatekos == "olló"
         and gep == "papír"
     ):
+        time.sleep(2)
         print("Gratulálok! Nyertél.")
         pontok[0] += 1
-    else:                       # ha nem a jatekos nyer
+    else:
+        time.sleep(2)                       # ha nem a jatekos nyer
         print("A gép nyert.")
         pontok[1] += 1
 
