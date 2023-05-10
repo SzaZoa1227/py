9cd7f1d0ab8dd9edb8cb192bfef4fe430be8cf66
SzaZoa1227 <116081320+SzaZoa1227@users.noreply.github.com>
2023-05-10 18:04:50 +0000

    kicsit gyorsabb igy

--- ido.py
+++ ido.py
@@ -1,23 +1,18 @@
-elsoOra= int(input("Az első időpont órája:   "))
-elsoPerc= int(input("Az első időpont perce:   "))
-masodikOra=int(input("Az második időpont órája:   "))
-masodikiPerc=int(input("Az második időpont perce:   "))
+elsoOra = int(input("Az első időpont órája: "))
+elsoPerc = int(input("Az első időpont perce: "))
+masodikOra = int(input("Az második időpont órája: "))
+masodikPerc = int(input("Az második időpont perce: "))
 
-def percbeAlakitas(ora):
-    return ora*60
+elsoIdo = elsoOra * 60 + elsoPerc
+# print(elsoIdo)
+masodikIdo = masodikOra * 60 + masodikPerc
+# print(masodikIdo)
+elteltIdo = masodikIdo - elsoIdo
+#print(elteltIdo)
 
-elsoIdo = elsoPerc+percbeAlakitas(elsoOra)
-masodikIdo = masodikPerc+percbeAlakitas(masodikOra)
-elteltIdo1 = elsoIdo-masodikIdo
-elteltIdo2 = elsoIdo-masodikIdo
-print(f"A két időpont között eltelt {elteltIdo1} perc.")
+orak = elteltIdo // 60
+# print(orak)
+percek = elteltIdo % 60
+# print(percek)
 
-def orabaAlakitas(idoPercben):
-    idoOraban = 0
-    while (idoPercben - 60) >= 60:
-        idoOraban +=1
-    return idoOraban
-for i in range(0, orabaAlakitas(elteltIdo1)):
-    elteltIdo -= 60
-
-print(f"A két időpont között eltelt {orabaAlakitas(elteltIdo1)} óra és {elteltIdo2} perc.")
+print(f"A két időpont között eltelt {orak} óra és {percek} perc.")
