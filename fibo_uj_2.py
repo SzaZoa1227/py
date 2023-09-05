1f0eb1e53eb23c422cf587bf4f332d3b93752dde
SzaZoa1227 <Szabozoard27@gmail.com>
2023-09-05 18:56:35 +0200

    felraktam mindent most

--- fibo_uj_2.py
+++ fibo_uj_2.py
@@ -1,16 +1,15 @@
 import sys
-sys.set_int_max_str_digits(99999)
 HANYADIK = int(input("Fibonacci sorozat hanyadik értékét írjuk ki?  "))
-if HANYADIK == 1:
-    print(f"A fibonacci sorozat {HANYADIK}. eleme a 0")
-    exit()
-elif HANYADIK == 2:
-    print(f"A fibonacci sorozat {HANYADIK}. eleme az 1")
-    exit()
-
-f1, f2 = 0, 1
-for i in range(2, HANYADIK+1):
-    F = f1 + f2
-    f1 = f2
-    f2 = F
-print(f"A fibonacci sorozat {HANYADIK}. eleme a(z): {F:,}")
+def fibo(HANYADIK):
+    if HANYADIK == 1:
+        return 0
+    elif HANYADIK == 2:
+        return 1
+    else:
+        f1, f2 = 0, 1
+        for i in range(2, HANYADIK+1):
+            F = f1 + f2
+            f1 = f2
+            f2 = F
+            return F
+print(f"A fibonacci sorozat {HANYADIK}. eleme a(z): {fibo(HANYADIK):,}")
