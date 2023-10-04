7e87d62011efb409f436dc99fca0334ac576a776
SzaZoa1227 <szabozoard27@gmail.com>
2023-10-04 12:10:49 +0200

    123

--- kpo.py
+++ kpo.py
@@ -38,7 +38,6 @@ while True:
         print("A gép nyert.")
         pontok[1] += 1
 
-    print("pontok: Te {} - {} Gép".format(pontok[0], pontok[1]))
-    ujjatek = input("Új játék? (I/N) ").lower()
-    if ujjatek != "i":
-        break
+    print(f"pontok: Te {pontok[0]} - {pontok[1]} Gép")
+    ujjatek = ""
+    
