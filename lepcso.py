3b97eefe2dfc3f49d88b542107327cc203e9c753
SzaZoa1227 <Szabozoard27@gmail.com>
2023-10-05 22:10:57 +0200

    yes

---
+++ lepcso.py
@@ -0,0 +1,3 @@
+for i in range(2,10,2):
+    print(i*" "+"|"+"_")
+print("|_\n  |_\n    |_\n      |_\n        |_\n          |_") #csakis azért van itt, mert Róland kihívást adott
---
+++ teknos.py
@@ -0,0 +1,4 @@
+import tkinter as TK
+import turtle
+from turtle import *
+forward(100)
